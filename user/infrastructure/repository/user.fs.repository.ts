31e40148f00f792b8bbc15c889b49f40733ed39d
Jacques Cornat <cornat.jacques@gmail.com>
2024-08-18 15:02:01 +0200

    add user fs repository tests

---
+++ user/infrastructure/repository/user.fs.repository.ts
@@ -0,0 +1,52 @@
+import { User, UserSerialized } from '../../domain/model/user.ts';
+import { UserRepository } from '../../domain/repository/user.repository.ts';
+import { NotFoundException, ParseErrorException } from '../../../shared/lib/exceptions.ts';
+
+export class UserFileSystemRepository implements UserRepository {
+  constructor(
+    public filePath = './config/user.json',
+  ) {}
+
+  async getByEmail(email: string): Promise<User> {
+    const userList = await this.getContent();
+    const user = userList.find((item) => item.email.value === email);
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
+  }
+
+  async save(user: User): Promise<void> {
+    const userList = await this.getContent();
+    userList.push(user);
+
+    const serializedFiles = userList.map((temp) => temp.serialize());
+    await Deno.writeTextFile(this.filePath, JSON.stringify(serializedFiles), { create: true });
+  }
+
+  private async getContent(): Promise<User[]> {
+    let content: string;
+
+    try {
+      content = await Deno.readTextFile(this.filePath);
+    } catch (error) {
+      if (!(error instanceof Deno.errors.NotFound)) {
+        throw error;
+      }
+
+      content = '[]';
+    }
+
+    let array: UserSerialized[];
+
+    try {
+      array = JSON.parse(content);
+    } catch (error) {
+      console.log(error);
+      throw new ParseErrorException();
+    }
+
+    return array.map((item) => User.reconstitute(item));
+  }
+}

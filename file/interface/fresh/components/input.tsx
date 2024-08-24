71fa4d196537a127d47500b90a7ce1eaa8907f9b
Jacques Cornat <cornat.jacques@gmail.com>
2024-08-24 19:49:56 +0200

    add icons

--- file/interface/fresh/components/input.tsx
+++ file/interface/fresh/components/input.tsx
@@ -4,7 +4,7 @@ export default function Input(props: JSX.HTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      class={`rounded-md w-full px-3.5 mb-4 py-2.5 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${props.class ?? ''}`}
+      class={`rounded-md w-full px-3.5 mb-4 py-2.5 text-sm font-semibold bg-white disabled:bg-slate-100 disabled:text-slate-500 disabled:cursor-not-allowed text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${props.class ?? ''}`}
     />
   );
 }

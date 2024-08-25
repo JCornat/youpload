04f19d7da5fe49ec68dbb8cbb34ccfd7b12ff11a
Jacques Cornat <cornat.jacques@gmail.com>
2024-08-25 23:59:29 +0200

    add delete account && fix user fs repository

---
+++ user/application/use-case/command/delete-account.use-case.ts
@@ -0,0 +1,34 @@
+import { UserRepository } from '../../../domain/repository/user.repository.ts';
+import { PasswordHashingProvider } from '../../../domain/provider/password-hashing.provider.ts';
+import { ArgumentInvalidException, NotMatchingPasswordException } from '../../../../shared/lib/exceptions.ts';
+
+export interface DeleteAccountCommand {
+  userId: string;
+  currentPassword: string;
+}
+
+export class DeleteAccountUseCase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly passwordHashingProvider: PasswordHashingProvider,
+  ) {}
+
+  async handle(deleteAccountCommand: DeleteAccountCommand): Promise<void> {
+    if (!deleteAccountCommand.userId) {
+      throw new ArgumentInvalidException('User id must be provided');
+    }
+
+    if (!deleteAccountCommand.currentPassword) {
+      throw new ArgumentInvalidException('Current password must be provided');
+    }
+
+    const user = await this.userRepository.get(deleteAccountCommand.userId);
+
+    const isPasswordValid = await this.passwordHashingProvider.compare(deleteAccountCommand.currentPassword, user.password.value);
+    if (!isPasswordValid) {
+      throw new NotMatchingPasswordException();
+    }
+
+    await this.userRepository.remove(deleteAccountCommand.userId);
+  }
+}

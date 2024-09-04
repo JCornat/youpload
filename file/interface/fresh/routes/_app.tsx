975081162da7011126f8ec8a2b797e4cc23d0333
Jacques Cornat <cornat.jacques@gmail.com>
2024-09-04 23:29:30 +0200

    add segmented button component & icon font

--- file/interface/fresh/routes/_app.tsx
+++ file/interface/fresh/routes/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component }: PageProps) {
         <title>YouPload</title>
         <link rel='stylesheet' href='/styles.css' />
         <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;600;700&amp;display=swap' rel='stylesheet' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' />
       </head>
       <body>
         <Component />

e05f4edeac9a888f2641e27a3525948a2ff1881d
Jacques Cornat <cornat.jacques@gmail.com>
2024-09-10 21:48:00 +0200

    update description

--- file/interface/fresh/components/footer.tsx
+++ file/interface/fresh/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div class='flex items-center justify-center mb-4 pt-8 pb-4 gap-4 text-slate-700'>
-      <a href='/account' class={'underline'}>Sources available on GitHub</a>
+      <a href='https://github.com/jcornat/youpload' target={'_blank'} class={'underline'}>Sources available on GitHub</a>
     </div>
   );
 }

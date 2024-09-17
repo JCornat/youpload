c51eccba58e3c041b36c722343912dc01d456d14
Jacques Cornat <cornat.jacques@gmail.com>
2024-09-18 00:47:29 +0200

    add docker configuration

--- interface/routes/_middleware.ts
+++ interface/routes/_middleware.ts
@@ -10,21 +10,21 @@ export interface State {
 }
 
 export async function handler(req: Request, ctx: FreshContext<State>) {
-  const cookies = getCookies(req.headers);
-  const auth = cookies.auth;
-  if (auth) {
-    const sessionRepository = new SessionFileSystemRepository();
-    const session = await sessionRepository.get(auth);
-    const userId = session.userId;
-    const userRepository = new UserFileSystemRepository();
-    try {
+  try {
+    const cookies = getCookies(req.headers);
+    const auth = cookies.auth;
+    if (auth) {
+      const sessionRepository = new SessionFileSystemRepository();
+      const session = await sessionRepository.get(auth);
+      const userId = session.userId;
+      const userRepository = new UserFileSystemRepository();
       const user = await userRepository.get(userId);
       ctx.state.isLoggedIn = true;
       ctx.state.userName = user.name.value;
       ctx.state.userId = user.id;
-    } catch {
-      //
     }
+  } catch {
+    //
   }
 
   return await ctx.next();

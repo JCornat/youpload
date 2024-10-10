41b82e8c629a5de598d35f87adfac26b1a69da8e
Jacques Cornat <cornat.jacques@gmail.com>
2024-10-10 19:25:18 +0200

    fix deno cron hang on build

--- interface/main.ts
+++ interface/main.ts
@@ -9,5 +9,20 @@ import '@std/dotenv';
 import { start } from '$fresh/server.ts';
 import manifest from './fresh.gen.ts';
 import config from './fresh.config.ts';
+import { FileMetadataFileSystemRepository } from '../app/file/infrastructure/repository/file-metadata.fs.repository.ts';
+import { FileStorageFileSystemProvider } from '../app/file/infrastructure/provider/file-storage.fs.provider.ts';
+import { DateStubProvider } from '../app/shared/infrastructure/provider/date.stub.provider.ts';
+import { RemoveExpiredFilesCron } from '../app/file/application/cron/remove-expired-file.cron.ts';
+
+const isBuildMode = Deno.args.includes('build');
+if (!isBuildMode) {
+  Deno.cron('Remove expired files cron job', '* * * * *', () => {
+    const fileMetadataRepository = new FileMetadataFileSystemRepository();
+    const fileStorageProvider = new FileStorageFileSystemProvider();
+    const dateProvider = new DateStubProvider();
+    const removeExpiredFilesCron = new RemoveExpiredFilesCron(fileMetadataRepository, fileStorageProvider, dateProvider);
+    removeExpiredFilesCron.execute();
+  });
+}
 
 await start(manifest, config);
--- interface/routes/_middleware.ts
+++ interface/routes/_middleware.ts
@@ -2,41 +2,11 @@ import { FreshContext } from '$fresh/server.ts';
 import { getCookies } from '@std/http/cookie';
 import { SessionFileSystemRepository } from '../../app/user/infrastructure/repository/session.fs.repository.ts';
 import { UserFileSystemRepository } from '../../app/user/infrastructure/repository/user.fs.repository.ts';
-import { RemoveExpiredFilesCron } from '../../app/file/application/cron/remove-expired-file.cron.ts';
-import { DateStubProvider } from '../../app/shared/infrastructure/provider/date.stub.provider.ts';
-import { FileStorageFileSystemProvider } from '../../app/file/infrastructure/provider/file-storage.fs.provider.ts';
-import { FileMetadataFileSystemRepository } from '../../app/file/infrastructure/repository/file-metadata.fs.repository.ts';
 
 export interface State {
   isLoggedIn: boolean;
   userName: string;
   userId: string;
-}
-
-export class Context {
-  private static context: Context;
-
-  public constructor() {
-    Deno.cron('Remove expired files cron job', '* * * * *', () => {
-      const fileMetadataRepository = new FileMetadataFileSystemRepository();
-      const fileStorageProvider = new FileStorageFileSystemProvider();
-      const dateProvider = new DateStubProvider();
-      const removeExpiredFilesCron = new RemoveExpiredFilesCron(fileMetadataRepository, fileStorageProvider, dateProvider);
-      removeExpiredFilesCron.execute();
-    });
-  }
-
-  public static async init() {
-    Context.context = new Context();
-  }
-
-  public static instance() {
-    if (this.context) {
-      return this.context;
-    } else {
-      throw new Error('Context is not initialized!');
-    }
-  }
 }
 
 export async function handler(req: Request, ctx: FreshContext<State>) {

4415f84310c84c30f046b83a6fdd5916d6444ddd
Jacques Cornat <cornat.jacques@gmail.com>
2025-03-24 00:00:52 +0100

    clean cron initialization

--- interface/main.ts
+++ interface/main.ts
@@ -9,19 +9,12 @@ import '@std/dotenv';
 import { start } from '$fresh/server.ts';
 import manifest from '@interface/fresh.gen.ts';
 import config from '@interface/fresh.config.ts';
-import { FileMetadataFileSystemRepository } from '@file/infrastructure/repository/file-metadata.fs.repository.ts';
-import { FileStorageFileSystemProvider } from '@file/infrastructure/provider/file-storage.fs.provider.ts';
-import { DateStubProvider } from '@shared/infrastructure/provider/date.stub.provider.ts';
-import { RemoveExpiredFilesCron } from '@file/application/cron/remove-expired-file.cron.ts';
+import { removeExpiredFilesCron } from '@file/application/cron/remove-expired-file.cron.ts';
 
 const isBuildMode = Deno.args.includes('build');
 if (!isBuildMode) {
-  Deno.cron('Remove expired files cron job', '* * * * *', () => {
-    const fileMetadataRepository = new FileMetadataFileSystemRepository();
-    const fileStorageProvider = new FileStorageFileSystemProvider();
-    const dateProvider = new DateStubProvider();
-    const removeExpiredFilesCron = new RemoveExpiredFilesCron(fileMetadataRepository, fileStorageProvider, dateProvider);
-    removeExpiredFilesCron.execute();
+  Deno.cron('Remove expired files cron job', '* * * * *', async () => {
+    await removeExpiredFilesCron.execute();
   });
 }
 
